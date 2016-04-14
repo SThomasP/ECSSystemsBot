576bda4edc7a7254099adc6499eb4c008a5aeb96
SThomasP <stp1g15@soton.ac.uk>
2016-04-14 13:08:14 +0100

    Nothing Much, need to be able to remove tags and write issues as a list

--- GetWebPage.py
+++ GetWebPage.py
@@ -3,4 +3,4 @@ for line in urlopen('https://secure.ecs.soton.ac.uk/status/'):
     line = line.decode('utf-8')  # Decoding the binary data to text.
     if 'Core Priority Devices' in line:  #look for 'Core Priority Devices' To find the line of text with the list of issues
         lineIWant = line
-if lineIWant <>''
+    
