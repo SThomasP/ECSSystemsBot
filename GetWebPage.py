eb66e40aecefb0f4e32049b123effbc441a5a54e
SThomasP <stp1g15@soton.ac.uk>
2016-04-14 20:16:51 +0100

    Writing Issues to logfile from web page, TODO: comparison to previous log file (log1.txt), tweet a new issue every 30 secs, and copy log0.txt to log1.txt

--- GetWebPage.py
+++ GetWebPage.py
@@ -19,6 +19,7 @@ for line in urlopen('https://secure.ecs.soton.ac.uk/status/'):
         linesIWant = line.split('Priority Devices')[2].split("<tr")
         linesIWant.pop()
         linesIWant.pop(0)
+        issues=[]
 for f in linesIWant:
     if  not 'border: 0px' in f:
         if 'machine' in f:
@@ -26,8 +27,11 @@ for f in linesIWant:
             if 'state_2' in f:
                 service=f.split('<td>')[2].split('</td>')[0]
                 problem=f.split('<td>')[3].split('</td>')[0]
-                ComposeTweet(service,machineName,problem)
+                issues.append(service+','+machineName+','+problem+'\n')
         elif 'state_2' in f:
             service=f.split('<td>')[1].split('</td>')[0]
             problem=f.split('<td>')[2].split('</td>')[0]
-            ComposeTweet(service,machineName,problem)
+            issues.append(service+','+machineName+','+problem+'\n')
+logfile=open('log0.txt','w')
+logfile.writelines(issues)
+logfile.close()
